f16e6cb9113f0f2197848901bd4f6220afe0526a
Mohit Singh <140334981+mohit-singh-13@users.noreply.github.com>
2025-01-21 23:29:38 +0530

    Update route.ts

--- apps/merchant-app/app/api/user/route.ts
+++ apps/merchant-app/app/api/user/route.ts
@@ -1,18 +1,1 @@
-import prisma from "@repo/db/client";
-import { NextResponse } from "next/server";
 
-export const GET = async () => {
-  await prisma.user.create({
-    data: {
-      name: "Rahul Singh",
-      email: "rs@gmail.com",
-      password: "asjdflkjsd",
-      number: "23432",
-    },
-  });
-
-  return NextResponse.json({
-    success: "true",
-    message: "Data saved successfully",
-  });
-};
